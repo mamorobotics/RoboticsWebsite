65df05e760e8f9fd5ff6f37846adbfad6d62ca2c
Dabuilda <107159084+TheMagnificentMiles@users.noreply.github.com>
2023-10-26 21:07:30 -0400

    Change to flex so max doesn't "remove" my powers

--- src/routes/error-page.jsx
+++ src/routes/error-page.jsx
@@ -1,14 +1,21 @@
 import { Form, redirect, Link } from "react-router-dom";
 import SideBar from "./sidebar";
 import TopBar from "./topbar";
+import BottomBar from "./bottombar";
 
 export default function Error() {
   return (
     <>
-      <SideBar />
-      <article>
-        <TopBar />
-      </article>
+      <div class="full-body">
+        <SideBar />
+        <div class="page-body">
+          <TopBar name="You shouldn't be here..." sub="Im sorry, but this page doesn't exist."/>
+          <article>
+            
+          </article>
+          <BottomBar />
+        </div>
+      </div>
     </>
   );
 }
