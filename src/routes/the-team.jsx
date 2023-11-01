a29d6cfe114819df0502cd4b8628f17e224ec206
Dabuilda <107159084+TheMagnificentMiles@users.noreply.github.com>
2023-11-01 12:49:46 -0400

    Change card stuf

--- src/routes/the-team.jsx
+++ src/routes/the-team.jsx
@@ -15,13 +15,13 @@ export default function TheTeam() {
             <h1 style={{marginTop: 0}}>Leadership: </h1>
             <div className="team-flex">
               <PersonCard src="./assets/team-images/max-lazarus.png" name="Max Lazarous" title="General Manager" 
-              description="Max is currently in his 3rd year of computer science and played a role in kick starting the MATE team at the high school once again. Max has also been a part of the FTC robotics club at MHS mostly contributing to the coding aspect of the team. He is also a part of the Mamaroneck High School sailing team, and enjoys spending time on the water."/>
-              <PersonCard src="./assets/team-images/miles-conto.png" name="Miles Contopoulos" title="Head of Coding" 
-              description="Miles is currently in his 3rd year of computer science and played a key role in designing and creating the code base of our MATE ROV. He enjoys coding in his free time and is a member of the varsity track team. During the winter season, Miles also programs on the MHS FTC robotics team."/>
-              <PersonCard src="./assets/team-images/alondra-avila.jpg" name="Alondra Avila" title="Head of Documentation/Outreach" 
-              description="Alondra is a junior and currently in her 4th year in the engineering elective in MHS. She has previously been an active member of the FTC team working and organizing the documentation necessary to compete. Outside of robotics Alondra has a leadership role in MHS women's engineering club."/>
+              description="Max is currently a junoir and has been a part of the team for 3 years, mostly contributing to the coding aspect of the team. He is also a part of the Mamaroneck High School sailing team."/>
+              <PersonCard src="./assets/team-images/alondra-avila.jpg" name="Alondra Avila" title="Head of Documentation & Outreach" 
+              description="Alondra is a senior and has been a part of the team for 4 years, working and organizing the documentation necessary to compete. Outside of robotics Alondra has a leadership role in MHS women's engineering club."/>
               <PersonCard src="./assets/team-images/elise-carpenter.jpg" name="Elise Carpenter" title="Head of Engineering" 
               description="[DESCRIPTION]"/>
+              <PersonCard src="./assets/team-images/miles-conto.png" name="Miles Contopoulos" title="Head of Coding" 
+              description="Miles is currently a junior and has been a part of the team for 3 years, primarily working on the coding branch. He is also a member of the varsity track team and enjoys coding in his free time."/>
             </div>
             <h1 style={{marginTop: 0}}>Engineering: </h1>
             <div className="team-flex">
@@ -38,6 +38,16 @@ export default function TheTeam() {
               <PersonCard src="./assets/team-images/alex-lin" name="Alex Lin" title="Engineer" 
               description="[DESRIPTION]"/>
               <PersonCard src="./assets/team-images/greg-mirando" name="Greg Mirando" title="Engineer" 
+              description="[DESRIPTION]"/>
+            </div>
+            <h1 style={{marginTop: 0}}>Coding: </h1>
+            <div className="team-flex">
+              <PersonCard src="./assets/team-images/sam-sklar" name="Sam Sklar" title="Engineer" 
+              description="[DESRIPTION]"/>
+            </div>
+            <h1 style={{marginTop: 0}}>Documentation & Outreach: </h1>
+            <div className="team-flex">
+              <PersonCard src="./assets/team-images/sam-sklar" name="Sam Sklar" title="Engineer" 
               description="[DESRIPTION]"/>
             </div>
           </article>
