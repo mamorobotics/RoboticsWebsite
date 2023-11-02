bb1ba0f56a69f30ccbc811f30d3e13ccc4f2d71d
maxjlaz1 <maxjlaz@icloud.com>
2023-11-02 11:19:28 -0400

    flipping card progress

--- src/routes/personcard.jsx
+++ src/routes/personcard.jsx
@@ -3,15 +3,31 @@ import React, { useState } from "react";
 
 export default function PersonCard({src, name, title, description}) {
   return (
+    // <>
+    //     <div className="personcard">
+    //       <img src={src} alt={name} width="200px" height="266px"></img>
+    //       <div className="persontitle">
+    //         <h1>{name}</h1>
+    //         <h2>{title}</h2>
+    //         <p>{description}</p>
+    //       </div>
+    //     </div>
+    // </>
     <>
+      <div className="wholeCard">
         <div className="personcard">
-          <img src={src} alt={name} width="200px" height="266px"></img>
-          <div className="persontitle">
-            <h1>{name}</h1>
-            <h2>{title}</h2>
-            <p>{description}</p>
+          <div className="personCardInner">
+            <div className="personCardFront">
+              <img src={src} alt={name} width="200px" height="266px"></img>
+              <h1>{name}</h1>
+              <h2>{title}</h2>
+            </div>
+            <div className="personCardBack">
+              <p>{description}</p>
+            </div>
           </div>
         </div>
+      </div>
     </>
   );
 }
