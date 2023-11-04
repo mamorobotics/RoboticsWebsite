c9865386bfeeda0e158fd3585a53f17db37cc5bd
Dabuilda <107159084+TheMagnificentMiles@users.noreply.github.com>
2023-11-03 20:48:22 -0400

    Responsiveness

--- src/routes/personcard.jsx
+++ src/routes/personcard.jsx
@@ -3,23 +3,15 @@ import React, { useState } from "react";
 
 export default function PersonCard({src, name, title, description}) {
   return (
-    // <>
-    //     <div className="personcard">
-    //       <img src={src} alt={name} width="200px" height="266px"></img>
-    //       <div className="persontitle">
-    //         <h1>{name}</h1>
-    //         <h2>{title}</h2>
-    //         <p>{description}</p>
-    //       </div>
-    //     </div>
-    // </>
     <>
       <div className="personcard">
         <div className="personCardInner">
           <div className="personCardFront">
             <img src={src} alt={name} width="200px" height="266px"></img>
-            <h1>{name}</h1>
-            <h2>{title}</h2>
+            <div className="personCardTitle">
+              <h1>{name}</h1>
+              <h2>{title}</h2>
+            </div>
           </div>
           <div className="personCardBack">
             <p>{description}</p>
