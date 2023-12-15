da9d4fdd196d2e7a25a037d5ea2df7384aab147b
Miles Contopoulos <107159084+TheMagnificentMiles@users.noreply.github.com>
2023-12-15 09:00:29 -0500

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <React.StrictMode basename="/RoboticsWebsite">
     <RouterProvider router={router} />
   </React.StrictMode>
 );
