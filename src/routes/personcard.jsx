3bbc991e7babcc7588c1563507678810ed2664ad
maxjlaz1 <maxjlaz@icloud.com>
2023-12-19 14:49:49 -0500

    resize test

--- src/routes/personcard.jsx
+++ src/routes/personcard.jsx
@@ -7,7 +7,7 @@ export default function PersonCard({src, name, title, description}) {
       <div className="personcard">
         <div className="personCardInner">
           <div className="personCardFront">
-            <img src={src} alt={name} width="200px" height="266px" style="object-fit: cover;"></img>
+            <img src={src} alt={name} max-width="200px" max-height="266px" width="100%" height="100%"></img>
             <div className="personCardTitle">
               <h1>{name}</h1>
               <h2>{title}</h2>
